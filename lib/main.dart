2ed08cf10921e556e2a4209a84d3195be353d4c3
Ed <ed@museli.app>
2021-10-01 09:54:15 +1000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:workmanager/workmanager.dart';
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    print("Native called background task: $task");
+    return Future.value(true);
+  });
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  await Purchases.setDebugLogsEnabled(true);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    _initialisePurchases();
+  }
+
+  Future _initialisePurchases() async {
+    // TODO: Set apiKey here and applicationId in app/build.gradle
+    await Purchases.setup("apiKey");
+  }
+
+  Future _executeTask() async {
+    await Workmanager()
+        .registerOneOffTask("1", "simpleTask"); //Android only (see below)
+  }
+
+  Future _purchasePackage() async {
+    final offerings = await Purchases.getOfferings();
+    final package = offerings.current!.availablePackages.first;
+    await Purchases.purchasePackage(package);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              onPressed: _executeTask,
+              child: Text('Execute Task'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              onPressed: _initialisePurchases,
+              child: Text('Initialise Purchases'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+              ),
+              onPressed: _purchasePackage,
+              child: Text('Purchase Package'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
